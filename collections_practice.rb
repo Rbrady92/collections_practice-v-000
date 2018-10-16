814d1566ea5d8aff0857412141b792c8bd7d2144
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 20:52:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,4 +9,15 @@ def sort_array_asc(arr)
     end
   end
 end
- 
+
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
