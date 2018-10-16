f79874616442dec297e1d94f9639ad05b601ed82
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 20:55:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,6 @@
 def sort_array_asc(arr)
   arr.sort do |a, b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
-    end
+    a <==> b
   end
 end
 
@@ -24,12 +18,6 @@ end
 
 def sort_array_char_count(arr)
   arr.sort do |a, b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    elsif a.length > b.length
-      1
-    end
+    a.length <==> b.length
   end
 end
